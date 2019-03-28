68a65bd66c4adfbeedb30e7589e29a4ec7cf492c
DenysAbramenkov <dabramenkov@amcbridge.com>
2019-03-28 15:12:11 +0200

    Add Kanban Board interface

---
+++ Issues Tracker/Controllers/BoardController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Issues_Tracker.Controllers
+{
+    public class BoardController : Controller
+    {
+
+        IssueTrackerEntities db = new IssueTrackerEntities();
+
+        public ActionResult Index(string projectName)
+        {
+            Project project;
+            if (!string.IsNullOrEmpty(projectName))
+            {
+                project = db.Projects.FirstOrDefault(p => p.Name == projectName);
+            }
+            else project = db.Projects.First();
+            ViewBag.Projects = new SelectList(db.Projects.Select(p => p.Name));
+            project.Issues = db.Issues.Where(i => i.Project.Name == project.Name).ToList();
+            return View(project);
+        }
+    }
+}
--- Issues Tracker/Controllers/IssueController.cs
+++ Issues Tracker/Controllers/IssueController.cs
@@ -38,42 +38,9 @@ namespace Issues_Tracker.Controllers
         [HttpPost]
         public ActionResult Index(Issue issue)
         {
-            if (issue.Id > 0)
-            {
-                Issue issueToUpdate = db.Issues.FirstOrDefault(x => x.Id == issue.Id) ;
-                issueToUpdate.Number = issue.Number;
-                issueToUpdate.PriorityId = issue.PriorityId;
-                issueToUpdate.ProjectId = issue.ProjectId;
-                issueToUpdate.StateId = issue.StateId;
-                issueToUpdate.AssigneeId = issue.AssigneeId;
-                issueToUpdate.Descripton = issue.Descripton;
-                issueToUpdate.Summary = issue.Summary;
-                issueToUpdate.Project = db.Projects.FirstOrDefault(x => x.Id == issue.ProjectId);
-                issueToUpdate.State = db.States.FirstOrDefault(x => x.Id == issue.StateId);
-                issueToUpdate.Priority = db.Priorities.FirstOrDefault(x => x.Id == issue.PriorityId);
-                issueToUpdate.User = db.Users.FirstOrDefault(x => x.Id == issue.AssigneeId);
-                db.SaveChanges();
-            }
-
-            else
-            {
-                Issue issueToCreate = new Issue();
-                issueToCreate.Number = issue.Number;
-                issueToCreate.PriorityId = issue.PriorityId;
-                issueToCreate.ProjectId = issue.ProjectId;
-                issueToCreate.StateId = issue.StateId;
-                issueToCreate.AssigneeId = issue.AssigneeId;
-                issueToCreate.Summary = issue.Summary;
-                issueToCreate.Descripton = issue.Descripton;
-                issueToCreate.Project = db.Projects.FirstOrDefault(x => x.Id == issue.ProjectId);
-                issueToCreate.State = db.States.FirstOrDefault(x => x.Id == issue.StateId);
-                issueToCreate.Priority = db.Priorities.FirstOrDefault(x => x.Id == issue.PriorityId);
-                issueToCreate.User = db.Users.FirstOrDefault(x => x.Id == issue.AssigneeId);
-                db.Issues.Add(issueToCreate);
-                db.SaveChanges();
-
-            }
+            GetUpdatedOrNewIssue(issue);
             return RedirectToAction("Index");
+
         }
 
         public ActionResult AddEditIssue(int issueId)
@@ -86,17 +53,55 @@ namespace Issues_Tracker.Controllers
                 Priorityes = new SelectList(db.Priorities, "Id", "Name")
             };
 
-            ViewBag.States = new SelectList(db.States, "Id", "Name");
-            ViewBag.Asignee = db.Users.Select(x => x.Name);
-            ViewBag.Projects = db.Projects.Select(x => x.Name);
-            ViewBag.Priorityes = db.Priorities.Select(x => x.Name);
-
             if (issueId > 0)
             {
                 issueView.Issue = db.Issues.FirstOrDefault((issue => issue.Id == issueId));
             }
 
             return PartialView("AddEditIssue", issueView);
+        }
+
+        private void GetUpdatedOrNewIssue(Issue issue)
+        {
+                Issue issueToUpdate = new Issue();
+                if (issue.Id > 0)
+                {
+                    issueToUpdate = db.Issues.FirstOrDefault(x => x.Id == issue.Id);
+                    issueToUpdate.Number = issue.Number;
+                    issueToUpdate.PriorityId = issue.PriorityId;
+                    issueToUpdate.ProjectId = issue.ProjectId;
+                    issueToUpdate.StateId = issue.StateId;
+                    issueToUpdate.AssigneeId = issue.AssigneeId;
+                    issueToUpdate.Descripton = issue.Descripton;
+                    issueToUpdate.Summary = issue.Summary;
+                    issueToUpdate.Project = db.Projects.FirstOrDefault(x => x.Id == issue.ProjectId);
+                    issueToUpdate.State = db.States.FirstOrDefault(x => x.Id == issue.StateId);
+                    issueToUpdate.Priority = db.Priorities.FirstOrDefault(x => x.Id == issue.PriorityId);
+                    issueToUpdate.User = db.Users.FirstOrDefault(x => x.Id == issue.AssigneeId);
+                    db.SaveChanges();
+                }
+
+                else
+                {
+                    issueToUpdate.Number = GetNewNumber();
+                    issueToUpdate.PriorityId = issue.PriorityId;
+                    issueToUpdate.ProjectId = issue.ProjectId;
+                    issueToUpdate.StateId = issue.StateId;
+                    issueToUpdate.AssigneeId = issue.AssigneeId;
+                    issueToUpdate.Summary = issue.Summary;
+                    issueToUpdate.Descripton = issue.Descripton;
+                    issueToUpdate.Project = db.Projects.FirstOrDefault(x => x.Id == issue.ProjectId);
+                    issueToUpdate.State = db.States.FirstOrDefault(x => x.Id == issue.StateId);
+                    issueToUpdate.Priority = db.Priorities.FirstOrDefault(x => x.Id == issue.PriorityId);
+                    issueToUpdate.User = db.Users.FirstOrDefault(x => x.Id == issue.AssigneeId);
+                    db.Issues.Add(issueToUpdate);
+                    db.SaveChanges();
+                }         
+        }
+
+        private int GetNewNumber()
+        {
+            return db.Issues.Count() + 1;
         }
     }
 }
